44757faa7663786d5df2092b9795b9d928b520ba
Harshaldangela <150448568+Harshaldangela@users.noreply.github.com>
2024-01-23 17:11:58 +0530

    Add files via upload

---
+++ stopwatch/index.js
@@ -0,0 +1,32 @@
+const watch=document.querySelector("#watch");
+let timer;
+let milliseconds=0;
+function startWatch(){
+    watch.classList.remove("paused");
+    clearInterval(timer);
+    timer=setInterval(()=>{
+        milliseconds +=10;
+        let dateTimer=new Date(milliseconds);
+        watch.innerHTML=
+        ("0" + dateTimer.getUTCHours()).slice(-2) + ":" +
+        ("0" + dateTimer.getUTCMinutes()).slice(-2) + ":" +
+        ("0" + dateTimer.getUTCSeconds()).slice(-2) + ":" +
+        ("0" + dateTimer.getUTCMilliseconds()).slice(-3,-1);
+    })
+}
+function puasedWatch(){
+    watch.classList.add("paused");
+    clearInterval(timer);
+}
+function resetWatch(){
+    watch.classList.remove("paused");
+    clearInterval(timer);
+    milliseconds=0;
+    watch.innerHTML="00:00:00:00";
+}
+document.addEventListener("click",(e)=>{
+    const el=e.target;
+    if(el.id === "start") startWatch();
+    if(el.id === "pause") puasedWatch();
+    if(el.id === "reset") resetWatch();
+})
